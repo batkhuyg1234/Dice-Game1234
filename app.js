8cfaa6c237ed3eb14503257cf58df6c94e161903
batkhuyg <sw10d236@csms.edu.mn>
2019-11-22 05:35:23 -0800

    Togloomnii ehnii uildel togloom ehelhed bvgd teg baih zereg bltgel ajil

--- app.js
+++ app.js
@@ -6,12 +6,10 @@ var scores = [0, 0];
 // тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundscore = 0;
 // шоо буусан аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
-var dice = Math.floor(Math.random() * 6 + 1);
+var diceNumber = Math.floor(Math.random() * 6 + 1);
 
-// document.querySelector("#score-0").textContent = dice;
-// document.querySelector("#score-1").textContent = dice;
-document.querySelector("#score -0").textContent = 0;
-document.querySelector("#score -1").textContent = 0;
-document.querySelector("#current -1").textContent = 0;
-document.querySelector("#current -0").textContent = 0;
+document.getElementById("#score-0").textContent = "0"; // ID gaar function haij bga nuhtsuld getElementById eniig ashiglna
+document.getElementById("#score-1").textContent = "0";
+document.getElementById("#current-0").textContent = "0";
+document.getElementById("#current-1").textContent = "0";
 document.querySelector(".dice").style.display = "none";
